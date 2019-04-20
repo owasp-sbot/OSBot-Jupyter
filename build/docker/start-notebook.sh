7dee393db1b5d2d3fcb8218cfc2674ee20a91441
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-20 14:25:59 +0100

    fixed buildspec

--- build/docker/start-notebook.sh
+++ build/docker/start-notebook.sh
@@ -1,5 +1,8 @@
 echo "**** starting notebook****"
-./ngrok http 8888 > /dev/null &
+./ngrok http --log stdout 8888 &
+
+#wget http://127.0.0.1:4040/api/tunnels ; cat tunnels
+#ps -aux
 
 jupyter notebook
 
