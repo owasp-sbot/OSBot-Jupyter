91c0aae8fbabe7b763007ff0076e6a974c1b2e6e
Dinis Cruz <dinis.cruz@owasp.org>
2023-01-26 23:41:46 +0000

    added show_png_from_graph method

--- osbot_jupyter/api_notebook/Jp_Puml.py
+++ osbot_jupyter/api_notebook/Jp_Puml.py
@@ -1,12 +1,15 @@
+from osbot_jupyter.api_notebook.Jp_Helper import Jp_Helper
 from osbot_utils.utils.Files import temp_file, parent_folder, file_exists, file_to_base64
 import subprocess
 
 PATH_PLANTUML_JAR = '/usr/local/bin/plantuml.jar'
 
+
 class Jp_Puml:
 
     def __init__(self, plantuml_jar=PATH_PLANTUML_JAR):
         self.plantuml_jar = plantuml_jar
+
 
     def exec_plantuml_jar(self, params=None):
         if type(params) is str:
@@ -14,7 +17,7 @@ class Jp_Puml:
         process_name = 'java'
         puml_params = [process_name, '-jar', self.plantuml_jar, '-Xmx2512m',
                        '-DPLANTUML_LIMIT_SIZE=8192'                       ] + (params or [])
-        subprocess.run(puml_params)
+        return subprocess.run(puml_params)
 
         # this was hanging the jupyter notebook cell, so using subprocess instead
         # exec_result  = exec_process(process_name, puml_params)
@@ -40,3 +43,11 @@ class Jp_Puml:
         if result.get('status') == 'ok':
             return file_to_base64(result.get('png_file'))
         return None
+
+    def show_png_from_graph(self, hb_jira_slack):
+        jira_graph_jql = hb_jira_slack.jira_graph_jql
+        jira_graph_jql.render_puml_from_jira_graph()
+        puml_code = jira_graph_jql.jira_graph.get_puml()
+        result    = self.puml_to_png(puml_code)
+        png_file  = result.get('png_file')
+        Jp_Helper().show_png_file_binary(png_file)
